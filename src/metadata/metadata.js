2ee27b12baa6f732c015508c1deb396a5a7ea052
Tuanvayn <vuanhtuan3.14@gmail.com>
2023-05-07 11:37:03 +0700

    Add: My experiences

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -2,100 +2,219 @@ module.exports = {
 	name: 'Tuan, Vu Anh (Anthony)',
 	title: 'Software Engineer | Java developer | Backend Developer',
 	facts: {
+		'Email': '<a href="mailto:vuanhtuan3.14@gmail.com"><i class="fa fa-envelope fact-icon"></i>vuanhtuan3.14@gmail.com</a>',
 		'Mobile': '<a href="tel:+84385061465"><i class="fa fa-phone fact-icon"></i>(+84)385061465</a>',
-		'Github': `<a href="https://github.com/tuanvayn"><i class="fa-brands fa-github fact-icon"></i>/tuanvayn</a>`,
-		'LinkedIn': `<a href="https://www.linkedin.com/in/tuanvayn/"><i class="fa-brands fa-linkedin fact-icon"></i>/tuanvayn</a>`,
-		'Email': '<a href="mailto:vuanhtuan3.14@gmail.com"><i class="fa fa-envelope fact-icon"></i>vuanhtuan3.14@gmail.com</a>'
+		'LinkedIn': '<a href="https://www.linkedin.com/in/tuanvayn/"><i class="fa-brands fa-linkedin fact-icon"></i>/tuanvayn</a>',
+		'Github': '<a href="https://github.com/tuanvayn"><i class="fa-brands fa-github fact-icon"></i>/tuanvayn</a>'
 	},
 	skills: [
 		['Java', 100],
 		['JS', 90],
 		['Python', 80],
-		['Tragedy', 70],
-		['Poetry', 60],
-		['Erotic', 50],
-		['Directing', 40],
-		['Literature', 30],
-		['Influence', 25],
+		['OracleDB', 70],
+		['Docker', 60],
+		['K8s', 50],
+		['Oracle Service Bus', 40],
+		['REST', 30],
+		['SOAP', 25],
+		['Git', 25],
 	],
 
 	company: [
 		{
 			cname: "VMO Holdings",
-			crole: "SWE",
-			cperiod: "Jun 2018",
+			crole: "Senior Java developer",
+			cperiod: "2022 - Present",
 			prj: [
 				{
-					title: 'Revenue and expenditure plan service management for General Department of State reserves',
-					period: 'Jan 2022 ~ August 2022 (8 months)',
-					skills: ['tragedy', 'philosophy', 'death'],
-					role: "SWE",
+					title: 'P2C',
+					period: 'Aug 2022 - Present',
+					skills: ['Java', 'Spring', 'SOAP', 'REST', 'Git'],
+					role: "Senior Java developer",
 					contents: `
-・Working directly with customers
+Worked as an outsourced Java developer for maintaining multiple sub-system projects of PCCW (Hongkong) client
 
-・Plan/Estimate timeline for team member
+- Collaborated with the in-house development team to understand project requirements and design specifications
 
-・Guide team member
+- Developed and implemented new features for the client's services using Java and related technologies, such as Spring and Hibernate
 
-・Involve in solution and detail designing
+- Conducted code reviews and testing to ensure high-quality deliverables
 
-・Implement
+- Worked with the project manager and team members to ensure timely delivery of project milestones
 
-・Code & debug
+- Communicated regularly with stakeholders to provide updates on progress and address any issues or concerns
+
+- Maintained and provided ongoing support for the client's services, including bug fixes and system upgrades
+
+- Completed the project on time and within budget, resulting in improved functionality and stability of the client's services.
 `
 				},
 				{
-					title: 'Twelfth Night',
-					period: '1600 - 1601',
-					skills: ['comedy', 'love'],
-					role: "SWE",
+					title: 'eRFX Systems',
+					period: 'Aug 2022 - Present',
+					skills: ['Java', 'Struts', 'REST', 'Git', 'JSP', 'Jquery'],
+					role: "Senior Java developer",
 					contents: `
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-		
-Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-		
-Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
-		`
+Worked as an outsourced Java developer for maintaining eRFX project using SQL Server, Java, and Struts 2 belong to Ascend Group (Thailand)
+
+- Collaborated with the in-house development team to understand project requirements and design specifications
+
+- Developed and implemented new features for the system using Java and related technologies, such as Struts 2 and JDBC, and utilized SQL Server for database management
+
+- Conducted code reviews and testing to ensure high-quality deliverables
+
+- Worked with the project manager and team members to ensure timely delivery of project milestones
+
+- Communicated regularly with stakeholders to provide updates on progress and address any issues or concerns
+
+- Completed the project on time and within budget, resulting in improved efficiency and automation of the eRFX process.
+`
 				}
 			]
 		},
 		{
-			cname: "Enao",
-			crole: "SWE",
-			cperiod: "Jun 2018",
+			cname: "Enterprise Nao",
+			crole: "Technical leader",
+			cperiod: "2018 - 2022",
 			prj: [
 				{
-					title: 'Revenue and expenditure plan service management for General Department of State reserves',
-					period: 'Jan 2022 ~ August 2022 (8 months)',
-					skills: ['tragedy', 'philosophy', 'death'],
-					role: "SWE",
+					title: 'Vietinbank E-Fast business customers banking service',
+					period: 'Jun 2021 - Aug 2022',
+					skills: ['Java', 'Spring', 'Hibernate', 'REST', 'OracleDB', 'Docker', 'K8s', 'Git'],
+					role: "Team lead",
 					contents: `
-- Working directly with customers
+Worked as an outsourced Java developer for rebuilding Vietinbank business customer banking service to microservices
 
-- Plan/Estimate timeline for team member
+- Collaborated with the in-house development team to understand project requirements and design specifications
 
-- Guide team member
+- Developed and implemented new microservices for the banking service using Java and related technologies, such as Spring Boot and Docker, Kubernetes
 
-- Involve in solution and detail designing
+- Conducted code reviews and testing to ensure high-quality deliverables
 
-- Implement
+- Worked with the project manager and team members to ensure timely delivery of project milestones
 
-- Code & debug
+- Communicated regularly with stakeholders to provide updates on progress and address any issues or concerns
+
+- Completed the project on time and within budget, resulting in improved efficiency and scalability of banking services for business customers.
 `
 				},
 				{
-					title: 'Twelfth Night',
-					period: '1600 - 1601',
-					skills: ['comedy', 'love'],
-					role: "SWE",
+					title: 'Vietinbank E-Connect',
+					period: 'May 2021 - Jun 2022',
+					skills: ['Java', 'Spring', 'Hibernate', 'REST', 'OracleDB', 'Docker', 'K8s', 'Git'],
+					role: "Team lead",
 					contents: `
-- Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+Worked as an outsourced Java developer for designing and building Vietinbank system which supports connections from other systems like Misa ERP, Grab, etc...
 
-- Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
- 
-	- Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
+- Collaborated with the in-house development team to understand project requirements and design specifications
+
+- Developed and implemented new features for the system using Java and related technologies, such as Spring and Hibernate
+
+- Conducted code reviews and testing to ensure high-quality deliverables
+
+- Worked with the project manager and team members to ensure timely delivery of project milestones
+
+- Communicated regularly with stakeholders to provide updates on progress and address any issues or concerns
+
+- Completed the project on time and within budget, resulting in improved efficiency and accuracy of financial transactions for the organization.
+`
+				},
+				{
+					title: 'National population database',
+					period: 'Mar 2020 - Jul 2021',
+					skills: ['Java', 'Spring', 'Hibernate', 'Oracle DB', 'Oracle Service Bus', 'SOAP', 'REST', 'Git'],
+					role: "Team lead",
+					contents: `
+Worked as an outsourced developer for designing and building National Population Database project under the Ministry of Public Security
+
+- Collaborated with the in-house development team to understand project requirements and design specifications
+
+- Developed and implemented new features for the database using Java and related technologies, such as JDBC and SQL
+
+- Conducted code reviews and testing to ensure high-quality deliverables
+
+- Communicated regularly with the project manager and team members to provide updates on progress and address any issues or concerns
+
+- Completed the project on time and within budget, resulting in improved accuracy and accessibility of population data for law enforcement agencies.
+
+- Supporting incoming connection from customer's client.
+`
+				},
+				{
+					title: 'Fraud detection on ATM transactions',
+					period: 'Oct 2019 - Mar 2020',
+					skills: ['Java', 'Python', 'Hibernate', 'Spring', 'Kafka', 'ML/AI libs', 'OracleDB', 'Docker', 'Git'],
+					role: "Software engineer",
+					contents: `
+Designed and built real-time fraud detection system for ATM transactions, resulting in a significant reduction in fraudulent activity.
+
+- Studied and implemented new fraud detection algorithms and rules using Java and related technologies, such as Sk-learn and Tree-based algorithms, Tibco Statica, Tibco Datascience,....
+
+- Collaborated with data analysts and fraud investigators to identify patterns and develop strategies for detecting and preventing fraud
+
+- Conducted performance tuning and optimization of the fraud detection system to handle large volumes of data in real-time
+
+- Worked with the project manager and team members to ensure timely delivery of project milestones
+
+- Communicated regularly with stakeholders to provide updates on progress and address any issues or concerns
+
+- Successfully delivered the project on time and within budget, resulting in improved fraud detection capabilities and reduced financial losses.
+`
+				},
+				{
+					title: 'In-house device management system',
+					period: 'Dec 2018 - Dec 2019 ',
+					skills: ['Java', 'C', 'Thing-boards', 'MQTT', 'Git'],
+					role: "Software engineer",
+					contents: `
+Collaborated with another startup for building the IOT application for managing in-house devices' status
+
+- Designed and developed new features for the system using Java and related technologies
+
+- Collaborated with the project manager and team members to identify requirements and design specifications
+
+- Conducted code reviews and testing to ensure high-quality deliverables
+
+- Optimized system performance through code refactoring and database tuning
+
+- Communicated regularly with stakeholders to provide updates on progress and address any issues or concerns
+
+- Successfully delivered the project on time and within budget, resulting in improved device management capabilities and increased productivity.
+`
+				},
+				{
+					title: 'Scraping user data',
+					period: 'Jan 2018 - Dec 2018',
+					skills: ['Java', 'Cassandra', 'Apache Storm', 'Selenium', 'SVN'],
+					role: "Software engineer",
+					contents: `
+Built the Java-based data platform that scrapes types of data from various sources, including social media platforms, websites, and search engines
+
+- Studied, suggested, implemented solutions for scraping tools and techniques such as Apache Storm streaming, Selenium, Headless browsers API, Facebook GraphAPI,... to the team
+
+- Collaborated with the project manager and team members to identify requirements and design specifications
+
+- Participated in daily stand-up meetings and provided updates on the status of assigned tasks
+
+- Communicated regularly with stakeholders to provide updates on progress and address any issues or concerns
+
+- Successfully delivered the project on time and within budget, resulting in improved device management capabilities and increased productivity.
+
+- Collaborating with cross-functional teams to develop requirements and designs, I delivered an improved device management system on time and within budget, resulting in increased productivity through regular communication with stakeholders.
 `
 				}
+			]
+		}
+	],
+	edu: [
+		{
+			period: "Sep 2015 - July 2018",
+			name: "Hanoi vocational College of Technology",
+			degree: "Higher Vocational Diploma",
+			note: [
+				"GPA: 8.4/10",
+				"#1 score of graduation project of Information Technology department",
+				"Teaching assistant for some subjects such as OOP, DSA, Java"
 			]
 		}
 	]
